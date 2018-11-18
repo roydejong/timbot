8e227582a6f4e90f99d9615d8d5a1d5664624a38
Roy de Jong <roy@softwarepunt.nl>
2018-11-18 07:20:13 +0100

    [timbot-v2] Database: Add upsert utils (insert/update generated queries)

--- src/Data/Db.js
+++ src/Data/Db.js
@@ -2,14 +2,31 @@ const Bsql3 = require('better-sqlite3');
 const Timbot = require('../Core/Timbot');
 const DbMigrator = require('./DbMigrator');
 
+/**
+ * Timbot sqlite database manager.
+ */
 class Db {
+    /**
+     * Initializes a new database manager for a given configuration.
+     *
+     * @param {config} config - Timbot config
+     */
     constructor(config) {
         this.config = config;
     }
 
+    /**
+     * Initializes the database connection.
+     */
     init() {
-        let dbPath = this.getPath();
-        let dbOptions = this.getOptions();
+        let dbPath = Db.DEFAULT_PATH;
+
+        let dbOptions = {
+            memory: false,
+            readonly: false,
+            fileMustExist: false,
+            timeout: 5000
+        };
 
         Timbot.log.i(_("[DB] Using database file: {0}", dbPath));
 
@@ -17,6 +34,9 @@ class Db {
         this.migrate();
     }
 
+    /**
+     * Closes the database connection.
+     */
     close() {
         if (this.connection) {
             this.connection.close();
@@ -26,6 +46,11 @@ class Db {
         }
     }
 
+    /**
+     * Migrates / upgrades the database as needed.
+     *
+     * @returns {boolean|*|void}
+     */
     migrate() {
         let migrator = new DbMigrator(this);
         let ok = migrator.run();
@@ -37,17 +62,158 @@ class Db {
         return ok;
     }
 
-    getPath() {
-        return Db.DEFAULT_PATH;
+    /**
+     * Helper function: Perform an insert or update query based on the data in an object via primary key.
+     *
+     * This function will generate a prepared statement with bound parameters.
+     * You must make sure all keys in the object are safe.
+     *
+     * @param {string} tableName - The table name to upsert data to.
+     * @param {object} data - Contains data to insert or update by column name.
+     * @param {string} [primaryKeyName] - Overrides the default primary key name ("id").
+     * @param {boolean} [forceInsert] - Forces insert and does not perform update check.
+     * @return {bool|number} - Returns false on error, the inserted ID if available, otherwise true on write OK.
+     */
+    insertOrUpdate(tableName, data, primaryKeyName, forceInsert) {
+        if (!primaryKeyName) {
+            primaryKeyName = "id";
+        }
+
+        // ---
+
+        let isInserting = false;
+
+        try {
+            if (forceInsert) {
+                // Forcing insert
+                isInserting = true;
+            } else if (typeof data[primaryKeyName] !== "undefined" && !!data[primaryKeyName]) {
+                // Need to insert, primary key is missing
+                isInserting = true;
+            } else {
+                // May need to insert, a PK is set so let's see if it exists
+                let cval = this.connection
+                    .prepare(`SELECT COUNT(*) AS cval FROM "${tableName}" WHERE "${primaryKeyName}" = ?;`)
+                    .get(data[primaryKeyName]).cval;
+
+                if (!cval || parseInt(cval) === 0) {
+                    // Need to insert, no hit for PK
+                    isInserting = true;
+                }
+            }
+        } catch (e) {
+            Timbot.log.e(_("[DB] Could not determine insert/update mode: {0}", e.message));
+            return false;
+        }
+
+        // ---
+
+        if (isInserting) {
+            return this.insert(tableName, data);
+        } else {
+            return this.update(tableName, data, primaryKeyName);
+        }
     }
 
-    getOptions() {
-        return {
-            memory: false,
-            readonly: false,
-            fileMustExist: false,
-            timeout: 5000
-        };
+    /**
+     * Helper function: Insert data from an object into a table.
+     * Generates a prepared statement with bound parameters.
+     *
+     * Security note: You must verify that the object keys are safe.
+     *
+     * @param {string} tableName - Name of the table to insert to.
+     * @param {object} data - Object containing data keyed by column name.
+     * @returns {number|bool} - Returns the last insert / auto increment ID, returns false on failure, true on success but no insert id available.
+     */
+    insert(tableName, data) {
+        try {
+            let sKeyList = "";
+            let sValueList = "";
+            let boundParams = [];
+
+            let _first = true;
+
+            Object.keys(data).forEach((key) => {
+                if (data.hasOwnProperty(key)) {
+                    if (_first) {
+                        _first = false;
+                    } else {
+                        sKeyList += ", ";
+                        sValueList += ", ";
+                    }
+
+                    sKeyList += "`" + key + "`";
+                    sValueList += "@" + key;
+
+                    boundParams.push(data[key]);
+                }
+            });
+
+            let sQuery = `INSERT INTO ${tableName} (${sKeyList}) VALUES (${sValueList});`;
+            Timbot.log.d(_("[DB] Generated insert query: {0}", sQuery));
+
+            let info = this.connection
+                .prepare(sQuery)
+                .run(data);
+
+            return parseInt(info.lastInsertRowid) || true;
+        } catch (e) {
+            Timbot.log.e(_("[DB] Unable to generate/run update query: {0}", e.message));
+            return false;
+        }
+    }
+
+    /**
+     * Helper function: Update data from an object into a table, for an existing record, based on PK.
+     * Generates a prepared statement with bound parameters.
+     *
+     * Security note: You must verify that the object keys are safe.
+     *
+     * @param {string} tableName - Name of the table to update in.
+     * @param {object} data - Object containing data keyed by column name.
+     * @param {string} primaryKeyName - The name of the primary key.
+     * @returns {bool} - True on success, false on error or if no rows were affected.
+     */
+    update(tableName, data, primaryKeyName) {
+        let pkValue = data[primaryKeyName];
+
+        if (!pkValue) {
+            Timbot.log.e(_("[DB] Can't run update without valid primary key ({0}, {1})", tableName, primaryKeyName));
+            return false;
+        }
+
+        try {
+            // UPDATE `table_name` SET
+            let sQuery = 'UPDATE `' + tableName + '` SET ';
+            let _first = true;
+
+            Object.keys(data).forEach((key) => {
+                if (data.hasOwnProperty(key)) {
+                    if (_first) {
+                        _first = false;
+                    } else {
+                        sQuery += ", ";
+                    }
+
+                    // `column_name` = @column_name
+                    sQuery += '`' + key + '` = @' + key;
+                }
+            });
+
+            // WHERE `pk_name` = @pkval
+            sQuery += ' WHERE `' + primaryKeyName + '` = @pkval LIMIT 1;';
+
+            Timbot.log.d(_("[DB] Generated update query: {0}", sQuery));
+
+            let info = this.connection
+                .prepare(sQuery)
+                .run(data);
+
+            return info.changes >= 1;
+        } catch (e) {
+            Timbot.log.e(_("[DB] Unable to generate/run update query: {0}", e.message));
+            return false;
+        }
     }
 }
 
