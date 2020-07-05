0fc6191731207ca0207b39b040636fbf2e3f5a66
Roy de Jong <email@roydejong.net>
2020-07-05 21:46:32 +0200

    Live embeds: Keep stream title in "offline" variant

--- live-embed.js
+++ live-embed.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const moment = require('moment');
+const humanizeDuration = require("humanize-duration");
 
 class LiveEmbed {
   static createForStream(streamData) {
@@ -11,8 +13,9 @@ class LiveEmbed {
     if (isLive) {
       // Add status
       msgEmbed.setTitle(`:red_circle: **${streamData.user_name} is live on Twitch!**`);
-      msgEmbed.addField("Stream title", streamData.title, true);
-      msgEmbed.addField("Live status", isLive ? `Live for ${streamData.viewer_count} viewers` : 'Stream has now ended', true);
+
+      msgEmbed.addField("Title", streamData.title, true);
+      msgEmbed.addField("Status", isLive ? `Live with ${streamData.viewer_count} viewers` : 'Stream has ended', true);
 
       // Set thumbnail
       let thumbnailUrl = streamData.thumbnail_url;
@@ -21,9 +24,20 @@ class LiveEmbed {
       let thumbnailBuster = (Date.now() / 1000).toFixed(0);
       thumbnailUrl += `?t=${thumbnailBuster}`;
       msgEmbed.setImage(thumbnailUrl);
+
+      // Add uptime
+      let now = moment();
+      let startedAt = moment(streamData.started_at);
+
+      msgEmbed.addField("Uptime", humanizeDuration(now - startedAt, {
+        delimiter: ", ",
+        largest: 2
+      }));
     } else {
       msgEmbed.setTitle(`:white_circle: ${streamData.user_name} was live on Twitch.`);
       msgEmbed.setDescription('The stream has now ended.');
+
+      msgEmbed.addField("Title", streamData.title, true);
     }
 
     return msgEmbed;
