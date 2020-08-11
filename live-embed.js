9e1998232e6f91f05ad3089300637618c08375bd
Roy de Jong <email@roydejong.net>
2020-08-11 16:13:14 +0200

    Live embed: Support game boxart as thumbnail via config flag (closes #12)

--- live-embed.js
+++ live-embed.js
@@ -1,35 +1,55 @@
 const Discord = require('discord.js');
 const moment = require('moment');
 const humanizeDuration = require("humanize-duration");
+const config = require('./config.json');
 
 class LiveEmbed {
   static createForStream(streamData) {
     const isLive = streamData.type === "live";
+    const allowBoxArt = config.twitch_use_boxart;
 
     let msgEmbed = new Discord.MessageEmbed();
     msgEmbed.setColor(isLive ? "#9146ff" : "GREY");
     msgEmbed.setURL(`https://twitch.tv/${streamData.user_name.toLowerCase()}`);
-    msgEmbed.setThumbnail(streamData.profile_image_url);
+
+    // Thumbnail
+    let thumbUrl = streamData.profile_image_url;
+
+    if (allowBoxArt && streamData.game && streamData.game.box_art_url) {
+      thumbUrl = streamData.game.box_art_url;
+      thumbUrl = thumbUrl.replace("{width}", "288");
+      thumbUrl = thumbUrl.replace("{height}", "384");
+    }
+
+    msgEmbed.setThumbnail(thumbUrl);
 
     if (isLive) {
+      // Title
       msgEmbed.setTitle(`:red_circle: **${streamData.user_name} is live on Twitch!**`);
       msgEmbed.addField("Title", streamData.title, false);
+    } else {
+      msgEmbed.setTitle(`:white_circle: ${streamData.user_name} was live on Twitch.`);
+      msgEmbed.setDescription('The stream has now ended.');
 
-      // Add game
-      if (streamData.game) {
-        msgEmbed.addField("Game", streamData.game.name, false);
-      }
+      msgEmbed.addField("Title", streamData.title, true);
+    }
 
+    // Add game
+    if (streamData.game) {
+      msgEmbed.addField("Game", streamData.game.name, false);
+    }
+
+    if (isLive) {
       // Add status
       msgEmbed.addField("Status", isLive ? `Live with ${streamData.viewer_count} viewers` : 'Stream has ended', true);
 
-      // Set thumbnail
-      let thumbnailUrl = streamData.thumbnail_url;
-      thumbnailUrl = thumbnailUrl.replace("{width}", "1280");
-      thumbnailUrl = thumbnailUrl.replace("{height}", "720");
+      // Set main image (stream preview)
+      let imageUrl = streamData.thumbnail_url;
+      imageUrl = imageUrl.replace("{width}", "1280");
+      imageUrl = imageUrl.replace("{height}", "720");
       let thumbnailBuster = (Date.now() / 1000).toFixed(0);
-      thumbnailUrl += `?t=${thumbnailBuster}`;
-      msgEmbed.setImage(thumbnailUrl);
+      imageUrl += `?t=${thumbnailBuster}`;
+      msgEmbed.setImage(imageUrl);
 
       // Add uptime
       let now = moment();
@@ -41,11 +61,6 @@ class LiveEmbed {
         round: true,
         units: ["y", "mo", "w", "d", "h", "m"]
       }), true);
-    } else {
-      msgEmbed.setTitle(`:white_circle: ${streamData.user_name} was live on Twitch.`);
-      msgEmbed.setDescription('The stream has now ended.');
-
-      msgEmbed.addField("Title", streamData.title, true);
     }
 
     return msgEmbed;
