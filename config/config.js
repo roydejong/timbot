c056ad9f6e6f75a4bf13e462d8be25c90fa407fe
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-06-13 17:47:02 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -6,13 +6,13 @@
     "<USER_1>": "everyone",
     "<USER_2>": "here"
   },
-  "discord_bot_token": "<REPLACE_DISCORD_BOT_TOKEN>",
+  "discord_bot_token": "<SET_ME>",
   "twitch_check_interval_ms": 60000,
   "twitch_use_boxart": true,
   "oauth2": {
-    "twitch_client_id": "REPLACE_TWITCH_CLIENT_ID",
-    "twitch_client_secret": "REPLACE_TWITCH_CLIENT_SECRET",
-    "twitch_oauth_token": "TWITCH_OAUTH_TOKEN",
+    "twitch_client_id": "<SET_ME>",
+    "twitch_client_secret": "<SET_ME>",
+    "twitch_oauth_token": "<SET_ME>",
     "twitch_oauth_cooldown": "06/15/2021-06:05:15-am"
   },
   "cleverbot_token": ""
