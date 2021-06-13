c94627852a70a735debdf2e3ac26b23de8a38ae0
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-06-13 16:56:41 +0200

    Updated config import
    Updated config import
    Changed twitch_client_id location to config/config.json

--- twitch-api.js
+++ twitch-api.js
@@ -1,5 +1,6 @@
+const configmain = require('./config/config.json');
 const axios = require('axios');
-const config = require('./config.json');
+const moment = require('moment');
 
 /**
  * Twitch Helix API helper ("New Twitch API").
@@ -8,7 +9,7 @@ class TwitchApi {
   static get requestOptions() {
     // Automatically remove "oauth:" prefix if it's present
     const oauthPrefix = "oauth:";
-    let oauthBearer = config.twitch_oauth_token;
+    let oauthBearer = configmain.oauth2.twitch_oauth_token;
     if (oauthBearer.startsWith(oauthPrefix)) {
       oauthBearer = oauthBearer.substr(oauthPrefix.length);
     }
@@ -16,7 +17,7 @@ class TwitchApi {
     return {
       baseURL: "https://api.twitch.tv/helix/",
       headers: {
-        "Client-ID": config.twitch_client_id,
+        "Client-ID": configmain.oauth2.twitch_client_id,
         "Authorization": `Bearer ${oauthBearer}`
       }
     };
@@ -26,9 +27,9 @@ class TwitchApi {
     const res = err.response || { };
 
     if (res.data && res.data.message) {
-      console.error('[TwitchApi]', 'API request failed with Helix error:', res.data.message, `(${res.data.error}/${res.data.status})`);
+      console.error('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][TwitchApi]', 'API request failed with Helix error:', res.data.message, `(${res.data.error}/${res.data.status})`);
     } else {
-      console.error('[TwitchApi]', 'API request failed with error:', err.message || err);
+      console.error('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][TwitchApi]', 'API request failed with error:', err.message || err);
     }
   }
 
