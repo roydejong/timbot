fda6bdd9ad5b1e3a53490ed0162b69938dce2790
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-06-13 17:26:56 +0200

    Created roles.json
    Created roles.json

---
+++ config/roles.js
@@ -0,0 +1,16 @@
+{
+    "admin": {
+        "admin1": "734785219034611753",
+        "admin2": "",
+        "admin3": "",
+        "admin4": "",
+        "admin5": ""
+    },
+    "normal": {
+        "role1": "",
+        "role2": "",
+        "role3": "",
+        "role4": "",
+        "role5": ""
+    }
+}
