1fcccd382d47822cf5812cfec404a3c720772c78
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-06-13 17:25:53 +0200

    Moved and Updated Config
    Moved config.js from ./ to ./config
    Updated Config with new settings

---
+++ config/config.js
@@ -0,0 +1,19 @@
+{
+  "prefix":"!",
+  "twitch_channels": "<USER_1>, <USER_2>",
+  "discord_announce_channel": "stream-announcements",
+  "discord_mentions": {
+    "<USER_1>": "everyone",
+    "<USER_2>": "here"
+  },
+  "discord_bot_token": "<REPLACE_DISCORD_BOT_TOKEN>",
+  "twitch_check_interval_ms": 60000,
+  "twitch_use_boxart": true,
+  "oauth2": {
+    "twitch_client_id": "REPLACE_TWITCH_CLIENT_ID",
+    "twitch_client_secret": "REPLACE_TWITCH_CLIENT_SECRET",
+    "twitch_oauth_token": "TWITCH_OAUTH_TOKEN",
+    "twitch_oauth_cooldown": "06/15/2021-06:05:15-am"
+  },
+  "cleverbot_token": ""
+}
