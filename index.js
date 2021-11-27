eff5ff8af246700291d045851dc469300785d772
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-11-27 19:31:51 +0100

    Update index.js
    Updated Code to work with Discord.js v13 and new/other dependencies
    Moved parts of the code to other files.

--- index.js
+++ index.js
@@ -1,354 +1,56 @@
-const configmain = require('./config/config.json');
-
-const axios = require('axios');
-const moment = require('moment');
-const Cleverbot = require('clevertype').Cleverbot;
-
+// DiscordJS
 const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
-global.discordJsClient = client;
+const { Intents, Collection } = Discord;
+const client = new Discord.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_BANS,
+        /*Intents.FLAGS.GUILD_INTEGRATIONS,*/
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
+    ], 
+    partials: ["GUILD_MEMBER"]
+});
+//Discord const
+require('dotenv').config();
+const chalk = require('chalk');
+const timeFormat = 'yyyy'+'/'+'LL'+'/'+'dd'+'-'+'h'+':'+'mm'+':'+'ss'+'-'+'a';
+const { DateTime } = require('luxon');
+console.log('[Time]', DateTime.utc().toISO(), '[UTC]');
 
-const TwitchMonitor = require("./twitch/twitch-monitor");
-const FooduseMonitor = require("./twitch/fooduse-monitor");
-const DiscordChannelSync = require("./twitch/discord-channel-sync");
-const ElizaHelper = require('./twitch/eliza');
-const LiveEmbed = require('./twitch/live-embed');
-const MiniDb = require('./twitch/minidb');
+//Start
+console.log(chalk.yellow('[NodeJS]' + chalk.white(' ▪ ▪ ▪ ▪ ▪ ', 'DiscordBot Start', ' ▪ ▪ ▪ ▪ ▪ ')));
 
-// --- Startup ---------------------------------------------------------------------------------------------------------
-console.log('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '] Timbot is starting.');
+    client.commands = new Collection();
+    client.cooldowns = new Collection();
+    ['admin_command_handler', 'event_handler'].forEach(handler =>{
+        require(`./TimCore/Commen/handlers/${handler}`)(client, chalk, Discord);
+    });
+    client.twitch = new Collection();
+    ['twitchrequest'].forEach(request =>{
+        require(`./TimCore/Twitch/twitchRequest/${request}.js`)(client, chalk, Discord);
+    });
+    ['twitch'].forEach(Live =>{
+        require(`./TimCore/Twitch/${Live}.js`)(client, chalk, Discord);
+    });
+    client.reaction = new Collection();
+    ['words'].forEach(wordreaction =>{
+        require(`./TimSys/commands/react/${wordreaction}.js`)(client, chalk, Discord);
+    });
 
-//Command Event Database handler
-client.commands = new Discord.Collection();
-client.events = new Discord.Collection();
-['command_handler', 'event_handler'].forEach(handler =>{
-    require(`./handlers/${handler}`)(client, Discord);
-})
-client.reaction = new Discord.Collection();
-['words'].forEach(wordreaction =>{
-    require(`./react/${wordreaction}.js`)(client, Discord);
-})
-client.twitchrequest = new Discord.Collection();
-require(`./utils/twitchRequest/twitchrequest.js`)(client, Discord);
+    //Login
+    client.login(process.env.TOKEN);
 
-// --- Cleverbot init --------------------------------------------------------------------------------------------------
-let cleverbot = null;
-
-if (configmain.cleverbot_token) {
-    cleverbot = new Cleverbot({
-        apiKey: configmain.cleverbot_token,
-        emotion: 0,
-        engagement: 0,
-        regard: 100
-    }, true);
-}
-
-// --- Discord ---------------------------------------------------------------------------------------------------------
-console.log('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '] Connecting to Discord...');
-
-let targetChannels = [];
-let emojiCache = { };
-
-let getServerEmoji = (emojiName, asText) => {
-    if (typeof emojiCache[emojiName] !== "undefined") {
-        return emojiCache[emojiName];
-    }
-
-    try {
-        let emoji = client.emojis.cache.find(e => e.name === emojiName);
-
-        if (emoji) {
-            emojiCache[emojiName] = emoji;
-
-            if (asText) {
-                return emoji.toString();
-            } else {
-                return emoji.id;
-            }
-        }
-    } catch (e) {
-        console.error(e);
-    }
-
-    return null;
-};
-global.getServerEmoji = getServerEmoji;
-
-let syncServerList = (logMembership) => {
-    targetChannels = DiscordChannelSync.getChannelList(client, configmain.discord_announce_channel, logMembership);
-};
-
-client.on('ready', () => {
-    console.log('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][Discord]', `Bot is ready; logged in as ${client.user.tag}.`);
-
-    // Init list of connected servers, and determine which channels we are announcing to
-    syncServerList(true);
-
-    // Keep our activity in the user list in sync
-    StreamActivity.init(client);
-
-    // Begin Twitch API polling
-    TwitchMonitor.start();
-
-    // Activate Food Use integration
-    FooduseMonitor.start();
+//Error listener
+client.on('unhandledRejection', error => {
+    console.log(chalk.yellow('[' + DateTime.utc().toFormat(timeFormat) + ']' + lang.error.unhandled), chalk.white(error));
+});
+client.on('shardError', error => {
+    console.log(chalk.yellow('[' + DateTime.utc().toFormat(timeFormat) + ']' + lang.error.websocket), chalk.white(error));
 });
 
-client.on("guildCreate", guild => {
-    console.log('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][Discord]', `Joined new server: ${guild.name}`);
-
-    syncServerList(false);
-});
-
-client.on("guildDelete", guild => {
-    console.log('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][Discord]', `Removed from a server: ${guild.name}`);
-
-    syncServerList(false);
-});
-
-console.log('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][Discord]', 'Logging in...');
-client.login(configmain.discord_bot_token);
-
-// Activity updater
-class StreamActivity {
-    /**
-     * Registers a channel that has come online, and updates the user activity.
-     */
-    static setChannelOnline(stream) {
-        this.onlineChannels[stream.user_name] = stream;
-
-        this.updateActivity();
-    }
-
-    /**
-     * Marks a channel has having gone offline, and updates the user activity if needed.
-     */
-    static setChannelOffline(stream) {
-        delete this.onlineChannels[stream.user_name];
-
-        this.updateActivity();
-    }
-
-    /**
-     * Fetches the channel that went online most recently, and is still currently online.
-     */
-    static getMostRecentStreamInfo() {
-        let lastChannel = null;
-        for (let channelName in this.onlineChannels) {
-            if (typeof channelName !== "undefined" && channelName) {
-                lastChannel = this.onlineChannels[channelName];
-            }
-        }
-        return lastChannel;
-    }
-
-    /**
-     * Updates the user activity on Discord.
-     * Either clears the activity if no channels are online, or sets it to "watching" if a stream is up.
-     */
-    static updateActivity() {
-        let streamInfo = this.getMostRecentStreamInfo();
-
-        if (streamInfo) {
-            this.discordClient.user.setActivity(streamInfo.user_name, {
-                "url": `https://twitch.tv/${streamInfo.user_name.toLowerCase()}`,
-                "type": "STREAMING"
-            });
-
-            console.log('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][StreamActivity]', `Update current activity: watching ${streamInfo.user_name}.`);
-        } else {
-            console.log('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][StreamActivity]', 'Cleared current activity.');
-
-            this.discordClient.user.setActivity(null);
-        }
-    }
-
-    static init(discordClient) {
-        this.discordClient = discordClient;
-        this.onlineChannels = { };
-
-        this.updateActivity();
-
-        // Continue to update current stream activity every 5 minutes or so
-        // We need to do this b/c Discord sometimes refuses to update for some reason
-        // ...maybe this will help, hopefully
-        setInterval(this.updateActivity.bind(this), 5 * 60 * 1000);
-    }
-}
-
-// ---------------------------------------------------------------------------------------------------------------------
-// Live events
-
-let liveMessageDb = new MiniDb('live-messages');
-let messageHistory = liveMessageDb.get("history") || { };
-
-TwitchMonitor.onChannelLiveUpdate((streamData) => {
-    const isLive = streamData.type === "live";
-
-    // Refresh channel list
-    try {
-        syncServerList(false);
-    } catch (e) { }
-
-    // Update activity
-    StreamActivity.setChannelOnline(streamData);
-
-    // Generate message
-    const msgFormatted = `${streamData.user_name} went live on Twitch!`;
-    const msgEmbed = LiveEmbed.createForStream(streamData);
-
-    // Broadcast to all target channels
-    let anySent = false;
-
-    for (let i = 0; i < targetChannels.length; i++) {
-        const discordChannel = targetChannels[i];
-        const liveMsgDiscrim = `${discordChannel.guild.id}_${discordChannel.name}_${streamData.id}`;
-
-        if (discordChannel) {
-            try {
-                // Either send a new message, or update an old one
-                let existingMsgId = messageHistory[liveMsgDiscrim] || null;
-
-                if (existingMsgId) {
-                    // Fetch existing message
-                    discordChannel.messages.fetch(existingMsgId)
-                      .then((existingMsg) => {
-                        existingMsg.edit(msgFormatted, {
-                          embed: msgEmbed
-                        }).then((message) => {
-                          // Clean up entry if no longer live
-                          if (!isLive) {
-                            delete messageHistory[liveMsgDiscrim];
-                            liveMessageDb.put('history', messageHistory);
-                          }
-                        });
-                      })
-                      .catch((e) => {
-                        // Unable to retrieve message object for editing
-                        if (e.message === "Unknown Message") {
-                            // Specific error: the message does not exist, most likely deleted.
-                            delete messageHistory[liveMsgDiscrim];
-                            liveMessageDb.put('history', messageHistory);
-                            // This will cause the message to be posted as new in the next update if needed.
-                        }
-                      });
-                } else {
-                    // Sending a new message
-                    if (!isLive) {
-                        // We do not post "new" notifications for channels going/being offline
-                        continue;
-                    }
-
-                    // Expand the message with a @mention for "here" or "everyone"
-                    // We don't do this in updates because it causes some people to get spammed
-                    let mentionMode = (config.discord_mentions && config.discord_mentions[streamData.user_name.toLowerCase()]) || null;
-
-                    if (mentionMode) {
-                        mentionMode = mentionMode.toLowerCase();
-
-                        if (mentionMode === "everyone" || mentionMode === "here") {
-                            // Reserved @ keywords for discord that can be mentioned directly as text
-                            mentionMode = `@${mentionMode}`;
-                        } else {
-                            // Most likely a role that needs to be translated to <@&id> format
-                            let roleData = discordChannel.guild.roles.cache.find((role) => {
-                                return (role.name.toLowerCase() === mentionMode);
-                            });
-
-                            if (roleData) {
-                                mentionMode = `<@&${roleData.id}>`;
-                            } else {
-                                console.log('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][Discord]', `Cannot mention role: ${mentionMode}`,
-                                  `(does not exist on server ${discordChannel.guild.name})`);
-                                mentionMode = null;
-                            }
-                        }
-                    }
-
-                    let msgToSend = msgFormatted;
-
-                    if (mentionMode) {
-                        msgToSend = msgFormatted + ` ${mentionMode}`
-                    }
-
-                    let msgOptions = {
-                        embed: msgEmbed
-                    };
-
-                    discordChannel.send(msgToSend, msgOptions)
-                        .then((message) => {
-                            console.log('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][Discord]', `Sent announce msg to #${discordChannel.name} on ${discordChannel.guild.name}`)
-
-                            messageHistory[liveMsgDiscrim] = message.id;
-                            liveMessageDb.put('history', messageHistory);
-                        })
-                        .catch((err) => {
-                            console.log('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][Discord]', `Could not send announce msg to #${discordChannel.name} on ${discordChannel.guild.name}:`, err.message);
-                        });
-                }
-
-                anySent = true;
-            } catch (e) {
-                console.warn('[' + moment.utc().format('MM/DD/YYYY-h:mm:ss-A') + '][Discord]', 'Message send problem:', e);
-            }
-        }
-    }
-
-    liveMessageDb.put('history', messageHistory);
-    return anySent;
-});
-
-TwitchMonitor.onChannelOffline((streamData) => {
-    // Update activity
-    StreamActivity.setChannelOffline(streamData);
-});
-
-// --- Common functions ------------------------------------------------------------------------------------------------
-String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.split(search).join(replacement);
-};
-
-String.prototype.spacifyCamels = function () {
-    let target = this;
-
-    try {
-        return target.replace(/([a-z](?=[A-Z]))/g, '$1 ');
-    } catch (e) {
-        return target;
-    }
-};
-
-Array.prototype.joinEnglishList = function () {
-    let a = this;
-
-    try {
-        return [a.slice(0, -1).join(', '), a.slice(-1)[0]].join(a.length < 2 ? '' : ' and ');
-    } catch (e) {
-        return a.join(', ');
-    }
-};
-
-String.prototype.lowercaseFirstChar = function () {
-    let string = this;
-    return string.charAt(0).toUpperCase() + string.slice(1);
-};
-
-Array.prototype.hasEqualValues = function (b) {
-    let a = this;
-
-    if (a.length !== b.length) {
-        return false;
-    }
-
-    a.sort();
-    b.sort();
-
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
+// //--------END--------//
